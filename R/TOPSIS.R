ea97263a609244f30d32a101b05f24e836eec005
Rafael Benitez <rbensua@gmail.com>
2020-09-08 10:57:40 +0200

    update 1

---
+++ R/TOPSIS.R
@@ -0,0 +1,167 @@
+TOPSIS <- function(x,
+                   directions,
+                   w,
+                   norm.method = c("norm", "gauss", "minmax"),
+                   ordered = FALSE,
+                   makefigure = TRUE){
+  
+  # Checking input data
+  
+  if(!is.data.frame(x)){
+    stop("Input data should be a data frame")
+  }else {
+    x <- as.data.frame(x)
+  }
+  
+  # Checking directions
+  
+  if (length(directions) != ncol(x)-1){
+    stop("Number of optimal directions should be equal to the number of criteria!")
+  }
+  
+  if ( any( !directions %in% c("min","max"))){
+    stop("Directions should be either 'max' or 'min'!")
+  }
+  
+  # Cheking weights
+  
+  if(any(w<0) | abs(sum(w) - 1) > 1e-8 ){
+    stop("Weights should be positive and add up to 1!")
+  }
+  
+  # Checking normalization method
+  norm.method <- match.arg(norm.method)
+  
+  ##
+  ## DEFINING THE NORMALIZATION FUNCTION 
+  ##
+  
+  
+  normalize <- function(x, method = c( "norm", "gauss", "minmax")){
+    method <- match.arg(method)
+    if (method == "norm"){
+      norm_x <-  apply(x, MARGIN = 2, function(y) {
+        res <- y/sqrt(sum(y^2))
+        return(unname(res))
+      }
+      )
+    } else if (method == "minmax"){
+      norm_x <- apply(x, MARGIN = 2, function (y){
+        res <- (y - min(y)) / diff(range(y))
+        return(unname(res))
+      })
+    } else {
+      norm_x <- apply(x, MARGIN = 2, function (y){
+        res <- (y - mean(y)) / sd(y)
+        return(unname(res))
+      })
+    }
+    return(norm_x)
+  }
+  
+  
+  # Normalizing data
+  
+  altnames <- apply(x, MARGIN = 1, FUN = function(y) y[1])
+  NormMat <- normalize(x[,-c(1)], method = norm.method)
+  
+  rownames(NormMat) <-   altnames
+  
+  
+  
+  #
+  # DEFINING OBJECTIVE DIRECTIONS
+  #
+  
+  
+  
+  obj_pos <- directions
+  obj_neg <- ifelse(obj_pos == "max", "min","max")
+  
+  mask_pos_max <- obj_pos == "max"
+  mask_pos_min <- obj_pos == "min"
+  mask_neg_max <- obj_neg == "max"
+  mask_neg_min <- obj_neg == "min"
+
+  #
+  #  DETERMINING THE IDEAL AND ANTI-IDEAL SOLUTIONS
+  #
+  #
+
+    idx_max <- max.col(t(NormMat))
+    idx_min <- max.col(-t(NormMat))
+    maximums <- diag(NormMat[idx_max,])
+    minimums <- diag(NormMat[idx_min,])
+  
+  
+  opt_pos <- maximums*mask_pos_max + minimums*mask_pos_min
+  opt_neg <- maximums*mask_neg_max + minimums*mask_neg_min
+  
+  ##
+  ## DEFINING THE DISTANCE FUNCTION (EUCLIDEAN)
+  ##
+  
+  
+  D <- function(w,direction = c("pos","neg"),i){
+    direction <- match.arg(direction)
+    if(direction == "pos"){
+      opt <- opt_pos
+    } else{
+      opt <- opt_neg
+    }
+    Dist <- sqrt(sum((w*NormMat[i,]-w*opt)^2))
+    return(Dist)
+  }
+  
+  ##
+  ## DEFINING THE SCORE FUNCTION
+  ##
+  
+  score <- function(w,i) {
+    r <- D(w, direction = "neg",i) / (D(w, direction = "neg",i) + D(w, direction = "pos",i))
+    return(r)
+  }
+  
+  ##
+  ## COMPUTING ALL THE SCORES
+  ##
+  ##
+  
+  TOPSIS_scores <- sapply(seq_along(altnames), function(k) score(w,k))
+  names(TOPSIS_scores) <- altnames
+  
+  #
+  # MERGING THE RESULTS IN A DATA.FRAME
+  #
+  
+  scores_DF <- cbind(x[,1],
+                     data.frame(TOPSIS = TOPSIS_scores,
+                                row.names = NULL
+                     )
+  )
+  colnames(scores_DF)[1] <- colnames(x)[1]
+  
+  if(ordered){
+    scores_DF <- scores_DF[sort(scores_DF$TOPSIS, decreasing = TRUE, index.return = TRUE)$ix,]
+  }
+  
+  
+  #
+  # PLOT FIGURE
+  #
+  
+  
+  if(makefigure){
+    scores_DF$Idx <- 1:nrow(scores_DF)
+    
+    
+    
+    p <-  ggplot(data = scores_DF, aes(x = Idx)) +
+      geom_line(aes( y = TOPSIS)) +
+      geom_point(aes( y = TOPSIS)) +
+      theme_classic() +
+      scale_x_continuous(breaks = 1:nrow(scores_DF), labels = scores_DF[,1]) +
+      xlab(colnames(scores_DF)[1]) + ylab("Score")
+    show(p)
+  }
+}
---
+++ R/data.R
@@ -0,0 +1,88 @@
+#' Data: E. Jacquet-Lagreze J. Siskos  (1981).
+#'
+#' A subjective preference system for ten reference cars (source of criteria informations: Sp~cml Salon. L'action automobile et touristique, No. 238, octobre 1980)
+#'
+#' @usage data("JLS_1981")
+#' @format Data frame (tibble) with 10 rows and 7 columns. The variables are:
+#' \describe{
+#'   \item{CarModel}{Model of the car to be evaluated.}
+#'   \item{MaxSpeed}{Maximum speed (km / h).}
+#'   \item{HP}{Horse Power (CV).}
+#'   \item{Space}{Space inside the car (m^2).}
+#'   \item{UrbanCons}{Consumption in town (l / 100 km).}
+#'   \item{HighCons}{Consumption at 120 km /h (l / 100 km).}
+#'   \item{Price}{Price in French francs.}
+#' }
+#' @source E. Jacquet-Lagreze and J. Siskos  (1981). "Assessing a set of additive utility functions for multicriteria decision-making, the UTA method", European Journal of Operational Research,  10(2), 151-164. \code{doi}: 10.1016/0377-2217(82)90155-2
+#'
+#' @author
+#' \strong{Vicente Liern Carrion} (\email{vicente.liern@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' \strong{Rafael Benitez} (\email{rafael.suarez@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' University of Valencia (Spain)
+
+"JLS_1981"
+
+
+#' Data: P. Annoni, L. Dijkstra and T. E. Hellman (2016).
+#'
+#' Components of the three dimensions used to obtain the Social Progress Index for European Regions in 2016.
+#'
+#' @usage data("SPI")
+#' @format Data frame (tibble) with 272 rows and 13 columns. The variables are:
+#' \describe{
+#'   \item{Region Code}{NUTS 1, 2 or 3 code for the European region to be evaluated.}
+#'   \item{Columns 2 - 5}{Components of the "Basic Human Needs" dimension.}
+#'   \item{Columns 6 - 9}{Components of the "Foundations of Wellbeing" dimension.}
+#'   \item{Columns 10 - 13}{Components of the "Opportunity" dimension.}
+#' }
+#' @source P. Annoni, L. Dijkstra and T. E. Hellman (2016). "The EU Regional SPI: a Measure of Social Progress in the EU Regions", Methodological Paper. \code{url}: http://ec.europa.eu/regional_policy/sources/information/maps/methodological_note_eu_spi_2016.pdf. Accesed on 2020-03-08
+#'
+#' @author
+#' \strong{Vicente Liern Carrion} (\email{vicente.liern@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' \strong{Rafael Benitez} (\email{rafael.suarez@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' University of Valencia (Spain)
+
+"SPI"
+
+#' Data: A. Oztel, A. Karakas, A. Ahmet (2018)
+#'
+#' Decision matrix for Green Star hotel selection problem in Turkey.
+#'
+#' @usage data("GShotels")
+#' @format Data frame (tibble) with 35 rows and 11 columns. The variables are:
+#' \describe{
+#'   \item{HOTEL}{Code of the hotel.}
+#'   \item{dSubway}{Distance to the closest subway station (km).}
+#'   \item{dSubway}{Distance to the Airport (km).}
+#'   \item{dCoachStation}{Distance to Coach Station (km).}
+#'   \item{dSutanSquare}{Distance to Sultan Ahmet Square (km).}
+#'   \item{Traffic}{Traffic intensity (1-4).}
+#'   \item{PriceTA.}{Price given in Trip Advisor platform }
+#'   \item{scTrivago}{Score given in Trivago platform (0-10)}
+#'   \item{scTripAdvisor}{Score given in Trip Advisor platform (1-5)}
+#'   \item{scHotels.com}{Score given in Hotels.com platform (0-10)}
+#'   \item{scGoogle}{Score given in Google.com (1-5).}
+#' }
+#' @source Oztel A, Karakas A, Ahmet A. 2018. Green Star Hotel Selection with Fuzzy TOPSIS Method:Case of Istanbul. In: Dorczak R, Arslan H, Musialik R, editors. Recent researches on socialsciences. First edit ed. Krakow: Institute of Public Affairs - Jagiellonian University; chap.Green Star; p. 473–485.
+#'
+#' @author
+#' \strong{Vicente Liern Carrion} (\email{vicente.liern@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' \strong{Rafael Benitez} (\email{rafael.suarez@@uv.es}).
+#' \emph{Department of Business Mathematics}
+#'
+#' University of Valencia (Spain)
+
+"GShotels"
+
+
+
